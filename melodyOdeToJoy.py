50f8378a611c02bf0acace8deba1aecda9cdc1cb
AJPfleger <alexander.julian.pfleger@cern.ch>
2022-09-27 22:47:23 +0200

    add: ode to joy

---
+++ melodyOdeToJoy.py
@@ -0,0 +1,68 @@
+bpm = 120
+basenote = concertPitch/iv.M6
+melody = [
+    (1,iv.M3), # bar 1
+    (1,iv.M3),
+    (1,iv.P4),
+    (1,iv.P5),
+    (1,iv.P5), # bar 2
+    (1,iv.P4),
+    (1,iv.M3),
+    (1,iv.M2),
+    (1,iv.P1), # bar 3
+    (1,iv.P1),
+    (1,iv.M2),
+    (1,iv.M3),
+    (1.5,iv.M3), # bar 4
+    (0.5,iv.M2),
+    (2,iv.M2),
+    (1,iv.M3), # bar 5
+    (1,iv.M3),
+    (1,iv.P4),
+    (1,iv.P5),
+    (1,iv.P5), # bar 6
+    (1,iv.P4),
+    (1,iv.M3),
+    (1,iv.M2),
+    (1,iv.P1), # bar 7
+    (1,iv.P1),
+    (1,iv.M2),
+    (1,iv.M3),
+    (1.5,iv.M2), # bar 8
+    (0.5,iv.P1),
+    (2,iv.P1),
+    (1,iv.M2), # bar 9
+    (1,iv.M2),
+    (1,iv.M3),
+    (1,iv.P1),
+    (1,iv.M2), # bar 10
+    (0.5,iv.M3),
+    (0.5,iv.P4),
+    (1,iv.M3),
+    (1,iv.P1),
+    (1,iv.M2), # bar 11
+    (0.5,iv.M3),
+    (0.5,iv.P4),
+    (1,iv.M3),
+    (1,iv.M2),
+    (1,iv.P1), # bar 12
+    (1,iv.M2),
+    (1,1/iv.P4),
+    (2,iv.M3),
+   # (1,iv.), # bar 13
+    (1,iv.M3),
+    (1,iv.P4),
+    (1,iv.P5),
+    (1,iv.P5), # bar 14
+    (1,iv.P4),
+    (1,iv.M3),
+    (1,iv.M2),
+    (1,iv.P1), # bar 15
+    (1,iv.P1),
+    (1,iv.M2),
+    (1,iv.M3),
+    (1.5,iv.M2), # bar 16
+    (0.5,iv.P1),
+    (2,iv.P1),
+]
+
