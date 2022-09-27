9386088a193429eaf1341f92fb470cf83487e71e
AJPfleger <alexander.julian.pfleger@cern.ch>
2022-09-27 23:06:40 +0200

    add: arpeggio

---
+++ melodyArpeggio.py
@@ -0,0 +1,20 @@
+import intervalls
+iv = intervalls.intervalls
+
+bpm = 120
+basenote = 440/iv.M6
+melody = [
+    (1,iv.P1),
+    (1,iv.M3),
+    (1,iv.P5),
+    (1/2,iv.O8),
+    (1/2,iv.pause),
+    (1/2,iv.O8),
+    (1/2,iv.pause),
+    (1/2,iv.O8),
+    (1/2,iv.pause),
+    (1,iv.O8),
+    (1,iv.P5),
+    (1,iv.M3),
+    (1.5,iv.P1),
+    ]
