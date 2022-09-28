6ca9e0ede67dcdbeb4ea5e63dc1c3bab0fc42c6b
AJPfleger <alexander.julian.pfleger@cern.ch>
2022-09-28 11:34:57 +0200

    add: pushMelodyThroughScales

--- melodygenerator.py
+++ melodygenerator.py
@@ -19,7 +19,7 @@ def createTone(notevalue, pitch, bpm,fs=44100):
     return np.sin(pitch * 2 * np.pi * t)
 
 # notevalue compared to bpm. Every beat is of length 1
-def createToneOvertones(notevalue, pitch, bpm,fs=44100):
+def createToneOvertones(notevalue, pitch, bpm, sound='piano', fs=44100):
 
     duration = notevalue * 60 / bpm
     samples =  int(fs * duration)
@@ -28,12 +28,34 @@ def createToneOvertones(notevalue, pitch, bpm,fs=44100):
     # we only calculate overtones if there is a pitch (= no pause)
     if pitch != 0:
         t = np.linspace(0, duration, int(fs * duration))
-        weight = 1/5
-        ot = 0
-        while pitch < fs/2:
-            tone += np.sin(pitch * 2 * np.pi * t) * weight**ot
-            ot += 1
-            pitch *= 2
+
+        if sound == 'test':
+            weight = 1/5
+            ot = 0
+            while pitch < fs/2:
+                tone += np.sin(pitch * 2 * np.pi * t) * weight**ot
+                ot += 1
+                pitch *= 2
+            tone /= max(tone)
+            return tone
+        elif sound == 'piano':
+            #weight = [0.98, 1, 0.68, 0.58, 0.2, 0.2, 0.08]
+            #otfreq = [1, 2, 3, 4, 5, 6, 7]
+            weight = [6.2, 2.2, 1, 0.3, 0.6, 0.3, 0.5]
+            otfreq = [1, 2, 3, 4, 5, 6, 7]
+        elif sound == 'oboe':
+            weight = [1, 0.98, 0.58, 0.44]
+            otfreq = [1, 2, 3, 4]
+        elif sound == 'violin':
+            weight = [1, 0.14, 0.12, 0.07]
+            otfreq = [1, 2, 3, 4]
+
+        for ot in range(len(otfreq)):
+            otpitch = otfreq[ot] * pitch
+            if otpitch > fs/2: break
+            tone += np.sin(otpitch * 2 * np.pi * t) * weight[ot]
+
+        # normalize
         tone /= max(tone)
 
     return tone
@@ -59,24 +81,50 @@ def fadeOut(note, t=0.9):
     
     return note * weight
 
-def generateWaveForm(melody,basenote=440,bpm=60,fs=44100):
+def generateWaveForm(melody,basenote=440,bpm=60,sound='piano',fs=44100):
     wf = createTone(0, 1, bpm, fs)
     for n in range(len(melody)):
         currentNote = melody[n]
-        note = createToneOvertones(currentNote[0], currentNote[1]*basenote, bpm, fs)
+        note = createToneOvertones(currentNote[0], currentNote[1]*basenote, bpm, sound, fs)
         wf = np.append(wf,fadeOut(note))
 
     return wf
 
+def pushMelodyThroughScales(melody, fLow, fCenter, fHigh, bpm, sound='piano', fs=44100):
+    waveComplete = np.zeros(1)
 
+    # go up
+    n = 0
+    base = fCenter
+    while base*np.max(melody,0)[1] < fHigh:
+        base = fCenter * 2**(n/12)
+        waveStep = generateWaveForm(melody,base,bpm,sound,fs)
+        waveComplete = np.append(waveComplete,waveStep)
+        n += 1
+        
+    # go down
+    n = 0
+    while base > fLow:
+        base = fCenter * 2**(n/12)
+        waveStep = generateWaveForm(melody,base,bpm,sound,fs)
+        waveComplete = np.append(waveComplete,waveStep)
+        n -= 1
+
+    return waveComplete
 
 fs = 44100 # sample rate
+concertPitch = 440 # a^1
 
 #from melody.melodyOdeToJoy import *
 from melody.melodyArpeggio import *
 #from melody.melodyAlternate import *
 
 
-wave = generateWaveForm(melody,basenote,bpm,fs)
+#wave = generateWaveForm(melody,basenote,bpm,'piano',fs)
+fLow = concertPitch * 2**(-12/12)
+fCenter = concertPitch * 2**(-7/12)
+fHigh = concertPitch * 2**(0/12)
+wave = pushMelodyThroughScales(melody, fLow, fCenter, fHigh, bpm*5, 'piano', fs)
+
 
 wavfile.write('melody.wav', fs, wave)
